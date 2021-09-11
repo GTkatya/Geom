da3fe164ace5330ccad893f1ecd81e6a2cad26b4
GuryanovaE <ekaterinaguryanova66@gmail.com>
2021-09-11 14:55:47 +0700

    Создали модульную структуру проекта.

---
+++ src/Tests/TestCMD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestCMD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
